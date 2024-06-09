42ad547cbf69971646e5278ae6801fa28d89974f
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-06-09 22:23:17 +0300

    refactor: Update Grid component to use BentoGrid for rendering grid items

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -4,17 +4,32 @@ import { gridItems } from "@/data";
 
 const Grid = () => {
   return (
-    <section>
-      <BentoGrid>
-        {gridItems.map((item) => (
-          <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
-            className={item.className}
-          />
-        ))}
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
       </BentoGrid>
     </section>
   );
