52e665c40826f76c8f110f12be97bf7f17c782c0
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-06-11 06:00:33 +0300

    refactor: Improve RecentProjects component layout and add image support

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -16,7 +16,21 @@ const RecentProjects = () => {
             key={id}
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
-            <PinContainer>{title}</PinContainer>
+            <PinContainer title={title} href={link}>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                  <img src="/bg.png" alt="bg-img" />
+                </div>
+                <img src={img} alt={title} className="z-10 absolute bottom-0" />
+              </div>
+              <h1 className=" font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {des}
+              </p>
+            </PinContainer>
           </div>
         ))}
       </div>
