c694351be460dd21bef8a5e6ceb2473708b8af8a
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-07-24 23:50:29 +0300

    refactor: Update Clients component layout and add recent projects section

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Clients() {
-  return <div>Clients</div>;
+  return (
+    <div className=" py-20" id="projects">
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className=" text-purple">recent projects</span>
+      </h1>
+      <div className=" flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10"></div>
+    </div>
+  );
 }
 
 export default Clients;
