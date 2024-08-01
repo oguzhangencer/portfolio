85bfb370d68a42d4caca911f3ab71e21245dda92
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-08-01 23:11:17 +0300

    refactor: Update Clients component heading text

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -4,8 +4,8 @@ function Clients() {
   return (
     <div className=" py-20" id="projects">
       <h1 className="heading">
-        A small selection of{" "}
-        <span className=" text-purple">recent projects</span>
+        Kind words from
+        <span className=" text-purple">satisfied clients</span>
       </h1>
       <div className=" flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10"></div>
     </div>
