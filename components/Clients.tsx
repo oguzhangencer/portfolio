6063152ad31224b505472fe84f53d57e8f9dae7c
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-08-01 23:35:50 +0300

    refactor: Update Clients component layout and add testimonials section

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { testimonials } from "@/data";
 
 function Clients() {
   return (
     <div className=" py-20" id="projects">
       <h1 className="heading">
         Kind words from
-        <span className=" text-purple">satisfied clients</span>
+        <span className=" text-purple"> satisfied clients</span>
       </h1>
-      <div className=" flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10"></div>
+      <div className=" flex flex-col items-center">
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+      </div>
     </div>
   );
 }
